40bac39ae8e4e85343d9634697193c3d520ae1a7
Manuel Torres <67908724+manueltorresc@users.noreply.github.com>
2021-07-27 13:43:35 -0400

    Update landing-zone-security.sh

--- landing-zone-security.sh
+++ landing-zone-security.sh
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -eu
+source ./variables.env
+set -xeu
 
 echo ""
 echo "Preparing to execute with the following values:"
