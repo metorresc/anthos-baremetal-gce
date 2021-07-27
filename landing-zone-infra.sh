75fd08a420a821822952e746129cb5c58534a3ba
Manuel Torres <67908724+manueltorresc@users.noreply.github.com>
2021-07-27 13:43:09 -0400

    Update landing-zone-infra.sh

--- landing-zone-infra.sh
+++ landing-zone-infra.sh
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eu
+source ./variables.env
+set -xeu
 
 echo ""
 echo "Preparing to execute with the following values:"
