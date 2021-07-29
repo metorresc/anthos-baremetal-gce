f7458b400f315e6eb38844cdb81d4ef86a0b17dd
Manuel Torres <manuelcarmona@google.com>
2021-07-29 16:54:23 -0400

    v1.0.1 adding support for ABM 1.7 & 1.8

---
+++ abm-1.7.1/landing-zone-security.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+source ./variables.env
+set -eu
+
+echo ""
+echo "Preparing to execute with the following values:"
+echo "==================================================="
+echo "Project ID: $PROJECT_ID"
+echo "Service Account Name: $SERVICE_ACCOUNT"
+echo "==================================================="
+echo ""
+echo "Continuing in 5 seconds. Ctrl+C to cancel"
+sleep 5
+
+check_svc_acct=`gcloud iam service-accounts list --filter "$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" | grep "$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" | wc -l | tr -d ' '`
+if [ "$check_svc_acct" = "0" ];then 
+  gcloud iam service-accounts create $SERVICE_ACCOUNT
+else
+  echo "Service Account already exists. Skipping"
+fi
+
+# Enable the required APIs
+echo ""
+echo "Enabling APIs"
+gcloud services enable \
+    anthos.googleapis.com \
+    anthosgke.googleapis.com \
+    cloudresourcemanager.googleapis.com \
+    container.googleapis.com \
+    gkeconnect.googleapis.com \
+    gkehub.googleapis.com \
+    serviceusage.googleapis.com \
+    stackdriver.googleapis.com \
+    monitoring.googleapis.com \
+    logging.googleapis.com
+
+# Binding permissions to the service account
+echo ""
+echo "Configuring privileges to service account"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/gkehub.connect"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/gkehub.admin"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/logging.logWriter"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/monitoring.metricWriter"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/monitoring.dashboardEditor"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/stackdriver.resourceMetadata.writer"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/iam.serviceAccountKeyAdmin"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/compute.osLogin"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/osconfig.serviceAgent"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/compute.securityAdmin"
---
+++ abm-1.7.1/script-for-vms/controlplane-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script must be executed from the Anthos Workstation
+echo "This script must be executed from the Cloud Shell" 
+set -eu
+
+echo ""
+echo "Copying script into $ABM_CP2"
+gcloud beta compute scp variables.env root@$ABM_CP2:~ --zone "$ZONE" --tunnel-through-iap --project "$PROJECT_ID"
+gcloud beta compute scp script-for-vms/controlplane-2-vxlan.sh root@$ABM_CP2:~ --zone "$ZONE" --tunnel-through-iap --project "$PROJECT_ID"
+
+# SSH into the VM as root
+gcloud beta compute ssh --zone "$ZONE" "root@$ABM_CP2"  --tunnel-through-iap --project "$PROJECT_ID" << EOF
+chmod +x controlplane-2-vxlan.sh
+./controlplane-2-vxlan.sh
+EOF
